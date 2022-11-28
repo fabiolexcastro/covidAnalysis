2965458b41882cee2506ff92b68e5c16f5bc757b
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-11-28 16:55:30 -0500

    aed data

--- 01 aed data.R
+++ 01 aed data.R
@@ -16,7 +16,7 @@ tble <- filter(tble, year1 != '2022')
 unique(tble$MPIOPDET); unique(tble$pdetcat)
 
 # Selecting the main variables --------------------------------------------
-vars <- c('key', 'mortalidad', 'Totalipm', 'year1', 'Analfabetismo', 'Bajologroeducativo', 'Barrerasaserviciosparacuidad', 'Barrerasdeaccesoaserviciosd', 'Desempleodelargaduración', 'Hacinamientocrítico',  'Inadecuadaeliminacióndeexcret', 'Inasistenciaescolar', 'Materialinadecuadodeparedese', 'Materialinadecuadodepisos', 'Rezagoescolar', 'Sinaccesoafuentedeaguamejo', 'Sinaseguramientoensalud', 'Trabajoinfantil', 'Trabajoinformal', 'pertenenciaétnica', 'sexo', 'pdetcat', 'nombredepartamento', 'nombremunicipio') 
+vars <- c('key', 'mortalidad', 'edad', 'Totalipm', 'year1', 'Analfabetismo', 'Bajologroeducativo', 'Barrerasaserviciosparacuidad', 'Barrerasdeaccesoaserviciosd', 'Desempleodelargaduración', 'Hacinamientocrítico',  'Inadecuadaeliminacióndeexcret', 'Inasistenciaescolar', 'Materialinadecuadodeparedese', 'Materialinadecuadodepisos', 'Rezagoescolar', 'Sinaccesoafuentedeaguamejo', 'Sinaseguramientoensalud', 'Trabajoinfantil', 'Trabajoinformal', 'pertenenciaétnica', 'sexo', 'pdetcat', 'nombredepartamento', 'nombremunicipio') 
 sort(colnames(tble))
 
 tble <- dplyr::select(tble, vars) # Selecionamos variables de interés
@@ -25,13 +25,16 @@ tble <- relocate(tble, key, nombredepartamento, nombremunicipio, year1)
 # Pertenencia etnica ------------------------------------------------------
 etnc <- tibble(pertenenciaetnica = c(1, 2, 3, 4, 5, 6), pertenenciaEtn = c('Indigena', 'ROM', 'Raizal', 'Palenquero', 'Negro', 'Otro'))
 
+# Edades ------------------------------------------------------------------
+
 # Agrupacion de los datos -------------------------------------------------
 indr <- tble %>% filter(year1 == '2020') %>% distinct(key, nombredepartamento, nombremunicipio, Totalipm, Analfabetismo, Bajologroeducativo, Barrerasaserviciosparacuidad, Barrerasdeaccesoaserviciosd, Desempleodelargaduración, Hacinamientocrítico, Inadecuadaeliminacióndeexcret, Inasistenciaescolar, Materialinadecuadodeparedese, Materialinadecuadodepisos, Rezagoescolar, Sinaccesoafuentedeaguamejo, Sinaseguramientoensalud, Trabajoinfantil, Trabajoinformal)
 indr[duplicated(indr$key),]
 
 tble %>%
-  dplyr::select(key, nombredepartamento, nombremunicipio, year1, )
-  group_by(key, nombredepartamento, nombremunicipio, year1) %>% 
+  dplyr::relocate(key, nombredepartamento, nombremunicipio, year1, pertenenciaétnica, edad, sexo, pdetcat) %>% 
+  rename(codigo = key) %>% 
+  group_by(codigo, nombredepartamento, nombremunicipio, year1, pertenenciaétnica, sexo, pdetcat) %>% 
   dplyr::summarise(Totalipm = mean(Totalipm), Analfabetismo = mean(Analfabetismo), Bajologroeducativo = mean(Bajologroeducativo),
                    Barrerasaserviciosparacuidad = mean(Barrerasaserviciosparacuidad), Barrerasdeaccesoaserviciosd = mean(Barrerasdeaccesoaserviciosd),
                    )
