e36d907d3eb2456b14433d8b886c99620d6df59a
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-11-28 17:02:42 -0500

    aed data

--- 01 aed data.R
+++ 01 aed data.R
@@ -26,6 +26,7 @@ tble <- relocate(tble, key, nombredepartamento, nombremunicipio, year1)
 etnc <- tibble(pertenenciaetnica = c(1, 2, 3, 4, 5, 6), pertenenciaEtn = c('Indigena', 'ROM', 'Raizal', 'Palenquero', 'Negro', 'Otro'))
 
 # Edades ------------------------------------------------------------------
+edad <- tibble(edad = c(0:18, 19:64, 65:120), edad_class = c(rep('Niñez', length(0:18)), rep('Adultez', length(19:64)), rep('Vejez', length(65:120)) ))
 
 # Agrupacion de los datos -------------------------------------------------
 indr <- tble %>% filter(year1 == '2020') %>% distinct(key, nombredepartamento, nombremunicipio, Totalipm, Analfabetismo, Bajologroeducativo, Barrerasaserviciosparacuidad, Barrerasdeaccesoaserviciosd, Desempleodelargaduración, Hacinamientocrítico, Inadecuadaeliminacióndeexcret, Inasistenciaescolar, Materialinadecuadodeparedese, Materialinadecuadodepisos, Rezagoescolar, Sinaccesoafuentedeaguamejo, Sinaseguramientoensalud, Trabajoinfantil, Trabajoinformal)
