3cc9c38af661a2291580cb5e9d8b67482b0fa885
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-01 16:39:23 -0500

    Update 02 spatial join.R

--- 02 spatial join.R
+++ 02 spatial join.R
@@ -14,3 +14,10 @@ shpf <- st_read('./SHP/MGN2018_MPIO_POLITICO/MGN_MPIO_POLITICO.shp')
 plot(st_geometry(shpf))
 colnames(shpf)
 colnames(tble)
+
+# To make the join --------------------------------------------------------
+
+# 2020 / 2021
+tble_2020 <- filter(tble, year1 == 2020)
+tble_2021 <- filter(tble, year1 == 2021)
+
