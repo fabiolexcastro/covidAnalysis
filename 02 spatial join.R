d66f20e6f5e65b0d10f8dfef22eeb7785d935de3
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-01 17:48:24 -0500

    Update 02 spatial join.R

--- 02 spatial join.R
+++ 02 spatial join.R
@@ -1,7 +1,7 @@
 
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(tidyverse, rgeos, gtools, sf, fs, haven, rgeoda, rnaturalearth, rnaturalearthdata)
+pacman::p_load(tidyverse, rgeos, gtools, sf, fs, haven, rgeoda, rnaturalearth, rnaturalearthdata, RColorBrewer)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
