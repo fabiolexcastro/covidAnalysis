7bdb62ce36baab6f002ca928d07003bc7111ea7f
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-12 17:35:07 -0500

    Update 04 check results moran univariado general.R

--- 04 check results moran univariado general.R
+++ 04 check results moran univariado general.R
@@ -13,14 +13,29 @@ m_20 <- st_read('gpkg/moran_fallecidosGral_2020.gpkg')
 m_21 <- st_read('gpkg/moran_fallecidosGral_2021.gpkg')
 
 # Make the count ----------------------------------------------------------
-makeCount <- function(shpf){
+makeCount <- function(shpf, year){
   
   shpf <- m_20 # Proof
+  year <- 2020
+  
   freq <- as.data.frame(table(shpf$clase))
   colnames(freq) <- c('categoria', 'cantidad_municipios')
   freq <- freq %>% arrange(desc(cantidad_municipios))
   
+  mpos <- shpf %>% 
+    filter(!clase %in% c('Sin significancia', 'Aislados')) %>% 
+    st_drop_geometry %>% 
+    dplyr::select(nombredepartamento, nombremunicipio, count, clase) %>% 
+    as_tibble() %>% 
+    arrange(clase) %>% 
+    mutate(anio = year)
+  
+  View(mpos)
+  
+  
   
 }
+
+
 
 
