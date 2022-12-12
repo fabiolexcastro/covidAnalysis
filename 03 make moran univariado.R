bf6f66d1d2355dee751bcc765828ee5e5a931f79
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-12 16:57:26 -0500

    Update 03 make moran univariado.R

--- 03 make moran univariado.R
+++ 03 make moran univariado.R
@@ -38,3 +38,27 @@ lbls <- mutate(lbls, clase = c('Sin significancia', 'Alto-Alto', 'Bajo-Bajo', 'B
 s_20 <- inner_join(s_20, lbls, by = 'cluster_num')
 names(mran_clrs) <- c('Sin significancia', 'Alto-Alto', 'Bajo-Bajo', 'Bajo-Alto', 'Alto-Bajo', 'Indefinido', 'Aislados')
 
+# To make the map 2020
+wrld <- ne_countries(scale = 50, returnclass = 'sf')
+g_mrn_2020 <- ggplot() + 
+  geom_sf(data = s_20, aes(fill = clase, col = clase), lwd = 0.2)+
+  scale_fill_manual(values = mran_clrs) +
+  scale_color_manual(values = mran_clrs, guide = 'none') +
+  geom_sf(data = dpts, fill = NA, col = 'grey60', lwd = 0.5) + 
+  geom_sf(data = wrld, fill = NA, col = 'grey60', lwd = 0.2) + 
+  coord_sf(xlim = ext(dpts)[1:2], ylim = ext(dpts)[3:4]) + 
+  ggtitle(label = 'Análisis LISA para fallecidos por COVID-19 en el año 2020') + 
+  labs(x = 'Lon', y = 'Lat', caption = 'INS - 2020', fill = 'Categoria') +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(family = 'serif'), 
+        axis.text.y = element_text(family = 'serif'), 
+        axis.title.x = element_text(family = 'serif'), 
+        axis.title.y = element_text(family = 'serif'), 
+        plot.title = element_text(family = 'serif', hjust = 0.5, face = 'bold'),
+        legend.position = 'bottom', 
+        legend.title = element_text(face = 'bold', family = 'serif'), 
+        legend.text = element_text(family = 'serif'))
+
+ggsave(plot = g_mrn_2020, filename = './png/maps/morn_2020.png', units = 'in', width = 7, height = 9, dpi = 300)
+
+
