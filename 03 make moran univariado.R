caee1f5be74901998ff4c4a8dd49063fae864500
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-12 16:29:44 -0500

    Update 03 make moran univariado.R

--- 03 make moran univariado.R
+++ 03 make moran univariado.R
@@ -11,4 +11,8 @@ options(scipen = 999, warn = -1)
 s_20 <- st_read('gpkg/total_mortalidad_2020.gpkg')
 s_21 <- st_read('gpkg/total_mortalidad_2021.gpkg')
 
+# To calculate the weigths ------------------------------------------------
+q_20 <- queen_weights(s_20, order = 1)
+q_21 <- queen_weights(s_21, order = 1)
+
 
