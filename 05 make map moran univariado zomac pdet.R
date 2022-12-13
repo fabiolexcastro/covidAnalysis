6156063b3ff8fc0627eee0f9207d28b9187622fe
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-13 17:47:57 -0500

    Update 05 make map moran univariado zomac pdet.R

--- 05 make map moran univariado zomac pdet.R
+++ 05 make map moran univariado zomac pdet.R
@@ -69,3 +69,30 @@ g_mrn_2020 <- ggplot() +
 
 ggsave(plot = g_mrn_2020, filename = './png/maps/morn_2020_zomacpdet.png', units = 'in', width = 7, height = 9, dpi = 300)
 
+# 2021 --------------------------------------------------------------------
+
+g_mrn_2021 <- ggplot() + 
+  geom_sf(data = m_21, aes(fill = clase, col = clase), lwd = 0.2)+
+  scale_fill_manual(values = mran_clrs, name = 'Clase') +
+  scale_color_manual(values = mran_clrs, guide = 'none') +
+  geom_sf(data = dpts, fill = NA, col = 'grey60', lwd = 0.5) + 
+  geom_sf(data = wrld, fill = NA, col = 'grey60', lwd = 0.2) + 
+  new_scale_fill() + 
+  new_scale_color() +
+  geom_sf(data = m_21_zm, aes(col = Tipo), fill = NA, lwd = 0.6) +
+  scale_color_manual(values = c('#C9D3B4', '#70687A', '#CDDD57'), name = 'Tipo') +
+  coord_sf(xlim = ext(dpts)[1:2], ylim = ext(dpts)[3:4]) + 
+  ggtitle(label = 'Análisis LISA para fallecidos por COVID-19 en el año 2021') + 
+  labs(x = 'Lon', y = 'Lat', caption = 'INS - 2021', fill = 'Categoria') +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(family = 'serif'), 
+        axis.text.y = element_text(family = 'serif'), 
+        axis.title.x = element_text(family = 'serif'), 
+        axis.title.y = element_text(family = 'serif'), 
+        plot.title = element_text(family = 'serif', hjust = 0.5, face = 'bold'),
+        legend.position = 'bottom', 
+        legend.title = element_text(face = 'bold', family = 'serif'), 
+        legend.text = element_text(family = 'serif')) +
+  guides(color=guide_legend(nrow = 2, byrow = TRUE))
+
+ggsave(plot = g_mrn_2021, filename = './png/maps/morn_2021_zomacpdet.png', units = 'in', width = 7, height = 9, dpi = 300)
