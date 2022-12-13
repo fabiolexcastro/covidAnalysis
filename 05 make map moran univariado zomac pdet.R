84b93ce54c3581933baacae2e9a3b6e3ecde8982
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-13 17:13:19 -0500

    Update 05 make map moran univariado zomac pdet.R

--- 05 make map moran univariado zomac pdet.R
+++ 05 make map moran univariado zomac pdet.R
@@ -23,4 +23,32 @@ dpts <- st_read('./SHP/MGN2018_DPTO_POLITICO/MGN_DPTO_POLITICO.shp')
 m_20 <- left_join(m_20, zmpd, by = c('MPIO_CCNCT' = 'id_espa')) 
 m_21 <- left_join(m_21, zmpd, by = c('MPIO_CCNCT' = 'id_espa')) 
 
+# -------------------------------------------------------------------------
+# To make the map ---------------------------------------------------------
+# -------------------------------------------------------------------------
 
+# 2020 --------------------------------------------------------------------
+
+mran_clrs <- clrs$color
+names(mran_clrs) <- clrs$clase
+
+g_mrn_2020 <- ggplot() + 
+  geom_sf(data = m_20, aes(fill = clase, col = clase), lwd = 0.2)+
+  scale_fill_manual(values = mran_clrs) +
+  scale_color_manual(values = mran_clrs, guide = 'none') +
+  geom_sf(data = dpts, fill = NA, col = 'grey60', lwd = 0.5) + 
+  geom_sf(data = wrld, fill = NA, col = 'grey60', lwd = 0.2) + 
+  coord_sf(xlim = ext(dpts)[1:2], ylim = ext(dpts)[3:4]) + 
+  ggtitle(label = 'Análisis LISA para fallecidos por COVID-19 en el año 2020') + 
+  labs(x = 'Lon', y = 'Lat', caption = 'INS - 2020', fill = 'Categoria') +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(family = 'serif'), 
+        axis.text.y = element_text(family = 'serif'), 
+        axis.title.x = element_text(family = 'serif'), 
+        axis.title.y = element_text(family = 'serif'), 
+        plot.title = element_text(family = 'serif', hjust = 0.5, face = 'bold'),
+        legend.position = 'bottom', 
+        legend.title = element_text(face = 'bold', family = 'serif'), 
+        legend.text = element_text(family = 'serif'))
+
+ggsave(plot = g_mrn_2020, filename = './png/maps/mapa_test.png', units = 'in', width = 7, height = 9, dpi = 300)
