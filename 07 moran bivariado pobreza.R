38e1df5de738fa359747d5a796aabd0ca8155666
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-14 17:13:58 -0500

    Update 07 moran bivariado pobreza.R

--- 07 moran bivariado pobreza.R
+++ 07 moran bivariado pobreza.R
@@ -14,6 +14,28 @@ colnames(tble)
 dplyr::select(tble, Totalipm)
 hist(tble$Totalipm)
 
+# Histogram ---------------------------------------------------------------
+g_hs_tt <- ggplot(data = tble %>% filter(!is.na(year1)), aes(x = Totalipm)) + 
+  geom_histogram(binwidth = 1) + 
+  facet_wrap(~year1) +
+  labs(x = 'Índice de pobreza multidimensional', y = 'Frecuencia', caption = 'INS') + 
+  ggtitle(label = 'Índice de pobreza multidimensional para mortalidad por COVID-19') +
+  theme_ipsum_ps() + 
+  theme(axis.text.x = element_text(family = 'serif'), 
+        axis.text.y = element_text(family = 'serif'), 
+        plot.caption = element_text(family = 'serif'),
+        axis.title.x = element_text(family = 'serif'), 
+        strip.text = element_text(family = 'serif'),
+        axis.title.y = element_text(family = 'serif'), 
+        plot.title = element_text(family = 'serif', hjust = 0.5, face = 'bold'),
+        legend.position = 'bottom', 
+        legend.title = element_text(face = 'bold', family = 'serif'), 
+        legend.text = element_text(family = 'serif'))
+
+dir_create('png/graphs')
+ggsave(plot = g_hs_tt, filename = 'png/graphs/histogram_totalipm_year.png', units = 'in', width = 11, height = 8, dpi = 300)
+
+
 # To make the group by (mpio) ---------------------------------------------
 
 
