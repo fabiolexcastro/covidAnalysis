f06aa2029aa4ed36447eb8aa6a8fda445a8fe7c8
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-14 17:19:48 -0500

    Update 07 moran bivariado pobreza.R

--- 07 moran bivariado pobreza.R
+++ 07 moran bivariado pobreza.R
@@ -22,22 +22,16 @@ g_hs_tt <- ggplot(data = tble %>% filter(!is.na(year1)), aes(x = Totalipm)) +
   labs(x = 'Índice de pobreza multidimensional', y = 'Frecuencia', caption = 'INS') + 
   ggtitle(label = 'Índice de pobreza multidimensional para mortalidad por COVID-19') +
   theme_ipsum_ps() + 
-  theme(axis.text.x = element_text(family = 'serif'), 
-        axis.text.y = element_text(family = 'serif'), 
-        plot.caption = element_text(family = 'serif'),
-        axis.title.x = element_text(family = 'serif'), 
-        strip.text = element_text(family = 'serif'),
-        axis.title.y = element_text(family = 'serif'), 
-        plot.title = element_text(family = 'serif', hjust = 0.5, face = 'bold'),
+  theme(axis.text.x = element_text(family = 'serif', size = 8), 
+        axis.text.y = element_text(family = 'serif', size = 8), 
+        plot.caption = element_text(family = 'serif', size = 7),
+        axis.title.x = element_text(family = 'serif', size = 9), 
+        strip.text = element_text(family = 'serif', size = 12, face = 'bold'),
+        axis.title.y = element_text(family = 'serif', size= 9), 
+        plot.title = element_text(family = 'serif', hjust = 0.5, size = 13, face = 'bold'),
         legend.position = 'bottom', 
         legend.title = element_text(face = 'bold', family = 'serif'), 
         legend.text = element_text(family = 'serif'))
 
 dir_create('png/graphs')
-ggsave(plot = g_hs_tt, filename = 'png/graphs/histogram_totalipm_year.png', units = 'in', width = 11, height = 8, dpi = 300)
-
-
-# To make the group by (mpio) ---------------------------------------------
-
-
-
+ggsave(plot = g_hs_tt, filename = 'png/graphs/histogram_totalipm_year.png', units = 'in', width = 7, height = 5, dpi = 300)
