579e6cfd2b985a12396dcbf5f90c8e759221004b
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-14 17:15:09 -0500

    Update 07 moran bivariado pobreza.R

--- 07 moran bivariado pobreza.R
+++ 07 moran bivariado pobreza.R
@@ -2,11 +2,12 @@
 
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(tidyverse, terra, rgeos, gtools, sf, fs, haven, glue, rgeoda, rnaturalearth, rnaturalearthdata, RColorBrewer, classInt)
+pacman::p_load(tidyverse, hrbrthemes, terra, rgeos, gtools, sf, fs, haven, glue, rgeoda, rnaturalearth, rnaturalearthdata, RColorBrewer, classInt)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
 options(scipen = 999, warn = -1)
+
 
 # Load data ---------------------------------------------------------------
 tble <- read_csv('tble/data_v1.csv')
