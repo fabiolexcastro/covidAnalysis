24d043c8bb491cf8eaf01ec1abaae3c5a6f82430
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-19 17:23:23 -0500

    Add files via upload

---
+++ 08 get statistics from bimoran.R
@@ -0,0 +1,13 @@
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(tidyverse, hrbrthemes, ggnewscale, terra, rgeos, gtools, sf, fs, haven, glue, rgeoda, rnaturalearth, rnaturalearthdata, RColorBrewer, classInt)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+sh20 <- st_read('gpkg/bimoran/bimoran_total_count_2020_totalipm.gpkg')
+sh21 <- st_read('gpkg/bimoran/bimoran_total_count_2021_totalipm.gpkg')
