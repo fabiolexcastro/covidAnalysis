9bb33554e80c4f52d035201eccfbefc639e264b9
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-12-19 16:12:21 -0500

    Update 07 moran bivariado pobreza.R

--- 07 moran bivariado pobreza.R
+++ 07 moran bivariado pobreza.R
@@ -93,3 +93,10 @@ ggsave(plot = gm_tl, filename = './png/maps/totalipm.png', units = 'in', width =
 # To run bimoran ----------------------------------------------------------
 shpf_lipm_20 <- inner_join(shpf_lipm, filter(gral, year1 == 2020)[,-6], by = c('MPIO_CCNCT' = 'codigo'))
 shpf_lipm_21 <- inner_join(shpf_lipm, filter(gral, year1 == 2021)[,-6], by = c('MPIO_CCNCT' = 'codigo'))
+
+# 2020 --------------------------------------------------------------------
+qnwg_20 <- queen_weights(shpf_lipm_20, order = 1)
+morn_20 <- local_bimoran(w = qnwg_20, df = st_drop_geometry(shpf_lipm_20[c('count', 'Totalipm')]))
+lbls_20 <- lisa_labels(morn_20)
+clrs_20 <- setNames(lisa_colors(morn_20), lbls)
+
