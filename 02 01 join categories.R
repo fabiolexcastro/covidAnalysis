e52ccdd5a578e02e7c3a1d509e38d4d6f3dcc565
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2023-04-17 18:10:16 -0500

    Update 02 01 join categories.R

--- 02 01 join categories.R
+++ 02 01 join categories.R
@@ -12,4 +12,16 @@ mpio <- st_read('G:/D/data/IGAC/MGN2018_MPIO_POLITICO/MGN_MPIO_POLITICO.shp')
 ctgr <- read_excel('datos/tbl/categoria municipios.xlsx')
 colnames(ctgr) <- c('cod', 'categoria')
 
+# Join between the shapefile and the table --------------------------------
+mpio_ctgr <- inner_join(mpio, ctgr, by = c('MPIO_CCNCT' = 'cod'))
+mpio_ctgr
+dir_create('datos/gpkg')
+st_write(mpio_ctgr, 'datos/gpkg/mpios_categoria.gpkg')
 
+# Tasa --------------------------------------------------------------------
+excel_sheets('datos/tbl/TASA MORTALIDAD MUNICIPIO.xlsx')
+rate <- read_excel('datos/tbl/TASA MORTALIDAD MUNICIPIO.xlsx', sheet = 'Hoja2')
+
+View(rate)
+rate <- mutate(rate, divi = as.numeric(DIVIPOLA), rate = as.numeric(TASAMORTALIDAD))
+rate[!complete.cases(rate),]
